425913ac8f767330925551ab60275db9d0ae634d
PulgaGit <visit.pulga@gmail.com>
2025-04-21 01:09:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // Future scripts can go here if needed
-console.log("EQDB Site Loaded");
+console.log("GNFC EQDB Hub Loaded");
+
+// Adding some interactivity to the widgets if needed
+const widgets = document.querySelectorAll('.widget');
+
+widgets.forEach(widget => {
+  widget.addEventListener('click', () => {
+    alert('You clicked a widget!');
+  });
+});
