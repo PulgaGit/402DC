21a4fcf40c84f838b14ebae1e63cb54ef244898f
PulgaGit <visit.pulga@gmail.com>
2025-04-21 01:54:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-// Future scripts can go here
-console.log("GNFC Equipment Hub loaded.");
+// Basic console log
+console.log("GNFC EQDB Hub Loaded");
+
+// Widget click feedback
+document.querySelectorAll('.widget').forEach(widget => {
+  widget.addEventListener('click', () => {
+    alert(`You clicked: ${widget.querySelector('h3').innerText}`);
+  });
+});
